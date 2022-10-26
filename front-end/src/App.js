06bb856740bcba41a0d685f88d25d661124595be
Avery Lindseth <alindsethsemail@gmail.com>
2022-10-26 01:56:38 -0500

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import Rides from './pages/RidePage.js';
 import Shops from './pages/ShopsPage.js';
 import Events from './pages/EventsPage.js';
 import Map from './pages/MapPage.js';
+import './index.css';
 
 function App() {
     return (
@@ -16,6 +17,10 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />}></Route>
                     <Route path='/tickets' element={<Tickets />}></Route>
+                    <Route path='/rides' element={<Rides />}></Route>
+                    <Route path='/shops' element={<Shops />}></Route>
+                    <Route path='/events' element={<Events />}></Route>
+                    <Route path='/map' element={<Map />}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
--- front-end/src/components/navbar.js
+++ front-end/src/components/navbar.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
+const shadowColor = '#7A0707';
+
 function Navbar() {
+    const navbox = {
+        backgroundColor: 'red',
+        height: '60px',
+        border: '1px solid ' + shadowColor
+    };
+    const navbarstyle = {
+        listStyleType: 'none',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+    }
     return (
-        <h1>Navbar</h1>
+        <div style={navbox}>
+            <ul style={navbarstyle}>
+                <NavButton link="/" text="Home" />
+                <NavButton link="/tickets" text="Buy Tickets" />
+                <NavButton link="/rides" text="Rides" />
+                <NavButton link="/shops" text="Shops and Restaurants" />
+                <NavButton link="/events" text="Events" />
+                <NavButton link="/map" text="Map" />
+            </ul>
+        </div>
+    );
+}
+
+function NavButton(props) {
+    const navbuttonstyle = {
+        display: 'block',
+        color: 'white',
+        textAlign: 'center',
+        padding: '10px 25px',
+        textDecoration: 'none',
+        fontSize: '24px',
+        borderStyle: "none solid none none",
+        borderWidth: '1px',
+        borderColor: shadowColor,
+        boxShadow: '1px 1px 3px 1px ' + shadowColor
+    };
+    return (
+        <li style={{float:'left'}} className='navbutton'>
+            <Link style={navbuttonstyle} to={props.link}>{props.text}</Link>
+        </li>
     );
 }
 
