cd381c8db773d743202f145cc7df4a14c8cbabbe
Avery Lindseth <alindsethsemail@gmail.com>
2022-10-26 00:34:55 -0500

    commit

--- front-end/src/pages/Layout.js
+++ front-end/src/pages/Layout.js
@@ -5,28 +5,47 @@ import headerimg from '../images/headerimg.jpg';
 
 function Layout() {
     return (
-        <header>
-            <TopHeader />
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </header>
+        <>
+            <header>
+                <TopHeader />
+                <Navbar />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <BottomFooter />
+            </footer>
+        </>
     );
 }
 
 function TopHeader() {
     const headerstyle = {
         border: '5px solid red',
-        height: '150px'
+        height: '180px',
+        position: 'relative',
+        textAlign: 'center'
+    };
+    const logostyle = {
+        position:'absolute',
+        top:'50%',
+        left:'50%',
+        transform:'translate(-50%,-50%)',
+        color: 'white',
+        fontSize: '70px',
+        fontFamily: "Bungee Spice",
+        textShadow: "8px 10px 0 black"
     };
     return (
         <div style={headerstyle}>
-            <img src={headerimg}></img>
+            <img src={headerimg} style={{width:'100%',height:'100%'}}></img>
+            <div style={logostyle}>Amusement Park</div>
         </div>
     );
 }
 
-function Footer() {
+function BottomFooter() {
     const footerstyle = {
         backgroundColor: 'red',
         height: '200px'
