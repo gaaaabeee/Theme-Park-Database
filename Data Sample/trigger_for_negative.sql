af5129b212bfcac930b10c54c506789bb06abee1
Vuong2344 <88819034+Vuong2344@users.noreply.github.com>
2022-11-02 20:16:09 -0500

    Add files via upload

---
+++ Data Sample/trigger_for_negative.sql
@@ -0,0 +1,13 @@
+CREATE DEFINER=`root`@`localhost` TRIGGER `customer_BEFORE_UPDATE` BEFORE UPDATE ON `customer` FOR EACH ROW BEGIN
+IF (new.height < 0.0) THEN
+	SET new.height = NULL;
+    END IF;
+END
+
+CREATE DEFINER=`root`@`localhost` TRIGGER `customer_BEFORE_INSERT` BEFORE INSERT ON `customer` FOR EACH ROW BEGIN
+	IF (new.height < 0.0) THEN
+	SET new.height = NULL;
+    END IF;
+END
+
+-- this trigger activates before update and insert to stop negative value for height
