76e49d278f7906ed6fe85c07a1c681f354212c41
Karenmajala <116045128+Karenmajala@users.noreply.github.com>
2022-11-05 14:16:28 -0500

    Update Layout.js

--- front-end/src/components/Layout.js
+++ front-end/src/components/Layout.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Navbar from './navbar.js';
-import headerimg from '../images/headerimg.jpg';
+import bottomimage from '../images/bottomimage.png';
+import header from '../images/header.png'; 
 import '../css/layout.css';
 
 //layout of every page, includes header, navbar, and footer
+//<div id='headerlogo'>Amusement Park</div>
 
 function TopHeader(props) {
     return (
         <header>
             <div id='topheader'>
-                <img src={headerimg} style={{width:'100%',height:'100%'}} alt='header'></img>
-                <div id='headerlogo'>Amusement Park</div>
+                <img src={header} style={{width:'1200px',height:'170px'}} alt='header'></img>
+        
             </div>
             <Navbar login={props.login}/>
         </header>
@@ -20,12 +22,18 @@ function TopHeader(props) {
 function BottomFooter() {
     return (
         <footer id='bottomfooter'>
-            <pre id='footertext'>
-                <span id='footerlogo'>Amusement Park</span><br />
+            <div id='footerimage'>
+                <img src={bottomimage} style={{ width:'100%',height:'100%'}} alt='bottom'></img>
+            </div>
+            <br />
+            <pre id='footertext' >
+            <span id='footerlogo'>HELLO WORLD</span><br />
                 123456 John Doe Ln, Houston, TX 77494<br />
-                832-111-1111        amusementpark@gmail.com
+                832-111-1111 | HelloWorldPark@gmail.com
             </pre>
-            <p style={{position:'absolute',bottom:'0%',left:'1%'}}>Hi there! This website was made by Vuong Nguyen, Avery Lindseth, Chuong Tran, Gabe Gauthier, and Karen Beck.</p>
+            
+            <br/>
+            <p style={{position:'absolute',bottom:'0%',left:'25%'}}>Hi there! This website was made by Vuong Nguyen, Avery Lindseth, Chuong Tran, Gabe Gauthier, and Karen Beck.</p>
         </footer>
     );
 }
